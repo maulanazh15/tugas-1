b2a13915de93409d2aa3ff8f99d6ac0b9a31ab3b
maulanazh15 <maulanazh15@gmail.com>
2023-09-04 16:57:45 +0700

    melengkapi method perkenalan

--- main.dart
+++ main.dart
@@ -2,14 +2,16 @@ class Mahasiswa {
   String nim;
   String nama;
   int tahunLahir;
-
+  int get usia => DateTime.now().year - this.tahunLahir;
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    print(
+        "Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021070", "Maulana Zhahran", 2002);
+  mhs.perkenalan();
 }
